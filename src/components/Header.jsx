591c41446a015c28bea163d46c68387a0febd28f
julius bade <julbade21@gmail.com>
2018-12-11 10:54:34 -0800

    fixed bug on responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
+import Topnav from './Topnav';
 
-function responsiveNavbar() {
-    console.log(responsiveNavbar)
-    let x = document.getElementById('myTopnav');
-    if (x.className === 'topnav') {
-      x.className += ' responsive';
-    }  else {
-      x.className = 'topnav';
-    }
-  }   
+
 
 function Header() {
+
   return (
     <div>
       <style jsx>{`
@@ -32,66 +26,11 @@ function Header() {
                 margin-bottom: 0px;
                 font-family: monospace;
             }
-            .topnav {
-                overflow: hidden;
-                background-color: white;
-                margin: 0 auto;
-                width: 100%;
-                font-size: 15px;
-                text-transform: uppercase;
-                display: flex;
-                justify-content: center;
-                
-                        
+            
+              form {
+                text-align: right;
+                margin-left: 20px;
               }
-              
-              .topnav a {
-                float: left;
-               
-                color: black;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-                font-size: 17px;
-              }
-              
-              .topnav a:hover {
-                background-color: #ddd;
-                color: black;
-              }
-              
-              .active {
-                background-color:  #ffe6e6;
-                color: white;
-              }
-              
-              .topnav .icon {
-                display: none;
-                color: black;
-              }
-              
-              @media screen and (max-width: 600px) {
-                .topnav a:not(:first-child) {display: none;}
-                .topnav a.icon {
-                  float: right;
-                  display: block;
-                }
-              }
-              
-              @media screen and (max-width: 600px) {
-                .topnav.responsive {position: relative;}
-                .topnav.responsive .icon {
-                  position: absolute;
-                  right: 0;
-                  top: 0;
-                }
-                .topnav.responsive a {
-                  float: none;
-                  display: block;
-                  text-align: left;
-                }
-              }
-
         `}</style>
       <header>
         <div id='header'>
@@ -99,23 +38,11 @@ function Header() {
           <p>For Fashion Lovers</p>
           <form>
               <input type="text" placeholder="Search"/>
-              <button className="btn btn warning">Search</button>
+              <button className="btn"><i className="fa fa-search"></i></button>
           </form>
           
         </div>
-        <div className="topnav" id="myTopnav">
-          <a href="/" className="active">Home</a>
-          <a href="#shop">Shop</a>
-          <a href="#contact">Contact</a>
-          <a href="#about">About</a>
-          <a href="#cart"><i className="fa fa-shopping-cart"></i></a>
-          <a href="javascript:void(0);" className="icon" onClick={responsiveNavbar}>
-            <i className="fa fa-bars"></i>
-          </a>
-        </div>
-
-          
-
+        <Topnav/>
       </header>
     </div>
   );
---
+++ src/components/Topnav.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+
+function responsiveNavbar() {
+    console.log(responsiveNavbar)
+    let x = document.getElementById('myTopnav')
+    if (x.className === 'topnav') {
+      x.className += ' responsive';
+    }  else {
+      x.className = 'topnav';
+    }
+  }   
+
+function Topnav () {
+    return (
+        <div>
+            <style>
+                {`
+                .topnav {
+                    overflow: hidden;
+                    background-color: white;
+                    margin: 0 auto;
+                    width: 100%;
+                    font-size: 15px;
+                    text-transform: uppercase;
+                    display: flex;
+                    justify-content: center;
+                    
+                            
+                  }
+                  
+                  .topnav a {
+                    float: left;
+                   
+                    color: black;
+                    text-align: center;
+                    padding: 14px 16px;
+                    text-decoration: none;
+                    font-size: 17px;
+                  }
+                  
+                  .topnav a:hover {
+                    background-color: #ddd;
+                    color: black;
+                  }
+                  
+                  .active {
+                    background-color:  #ffe6e6;
+                    color: white;
+                  }
+                  
+                  .topnav .icon {
+                    display: none;
+                    color: black;
+                  }
+                  
+                  @media screen and (max-width: 600px) {
+                    .topnav a:not(:first-child) {display: none;}
+                    .topnav a.icon {
+                      float: right;
+                      display: block;
+                    }
+                  }
+                  
+                  @media screen and (max-width: 600px) {
+                    .topnav.responsive {
+                      position: relative;
+                      display: inline-block;
+                    }
+                    .topnav.responsive .icon {
+                      display: none;
+                      right: 0;
+                      top: 0;
+                      
+                    }
+                    .topnav.responsive a {
+                      float: none;
+                      display: block;
+                      text-align: left;
+                      position: relative;
+                    }
+                  }
+                
+                `}
+            </style>
+            <div className="topnav" id="myTopnav">
+          <a href="/" className="active">Home</a>
+          <a href="#shop">Shop</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+          <a href="#cart"><i className="fa fa-shopping-cart"></i></a>
+          <a href="javascript:void(0);" className="icon" onClick={responsiveNavbar}>
+            <i className="fa fa-bars"></i>
+          </a>
+        </div>
+        </div>
+    )
+}
+
+export default Topnav;
